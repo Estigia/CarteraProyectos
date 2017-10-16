7ff597e85dbc1459a5b71e11621052d6131dff36
Melvin Juárez <malvinjazz@live.com>
2017-10-15 23:42:25 -0600

    Views of the project application have been updated. The views in the list have been paginated and the project list supports the filter by project name. Project list and project form templates has been finished.

--- projects/views.py
+++ projects/views.py
@@ -1,44 +1,84 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-from django.views.generic import TemplateView,ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import TemplateView, ListView
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView
+)
 
 from .models import Project, Budget, Entry
 
-#Prject views
 
+# Project views
 class ProjectListView(ListView):
     model = Project
     template_name = "projects/project_list.html"
     context_object_name = "projects"
+    paginate_by = 10
 
-class ProjectCreateView(CreateView):
+    def get_queryset(self):
+        qs = super(ProjectListView, self).get_queryset()
+
+        if self.request.GET.get('name'):
+            qs = qs.filter(name__icontains=self.request.GET.get('name'))
+
+        return qs
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'projects/project_detail.html'
+
+
+class ProjectCreateView(SuccessMessageMixin, CreateView):
     model = Project
     template_name = "projects/project_form.html"
+    success_message = '%(name)s se agregó correctamente.'
     fields = [
         'name',
         'status',
-        'location'
+        'location',
+        'project_type',
+        'attendant',
+        'snip',
+        'nog',
+        'smip',
+        'budget',
+        # 'user'
     ]
     success_url = reverse_lazy('projects:project_list')
 
-class ProjectUpdateView(UpdateView):
+
+class ProjectUpdateView(SuccessMessageMixin, UpdateView):
     model = Project
     template_name = "projects/project_form.html"
+    success_message = '%(name)s se modificó correctamente.'
     fields = [
         'name',
         'status',
-        'location'
+        'location',
+        'project_type',
+        'attendant',
+        'snip',
+        'nog',
+        'smip',
+        'budget',
+        # 'user'
     ]
     success_url = reverse_lazy('projects:project_list')
 
-#Budget views
 
+# Budget views
 class BudgetListView(ListView):
     model = Budget
     template_name = "projects/budget_list.html"
     context_object_name = "budgets"
+    paginate_by = 10
 
 
 class BudgetCreateView(CreateView):
@@ -49,6 +89,7 @@ class BudgetCreateView(CreateView):
     ]
     success_url = reverse_lazy('projects:budget_list')
 
+
 class BudgetUpdateView(UpdateView):
     model = Budget
     template_name = "projects/budget_form.html"
@@ -57,12 +98,14 @@ class BudgetUpdateView(UpdateView):
     ]
     success_url = reverse_lazy('projects:budget_list')
 
-#Entrys views
 
+# Entrys views
 class EntryListView(ListView):
     model = Entry
     template_name = "projects/entry_list.html"
     context_object_name = "entrys"
+    paginate_by = 10
+
 
 class EntryCreateView(CreateView):
     model = Entry
@@ -74,6 +117,7 @@ class EntryCreateView(CreateView):
         "unit_cost",
     ]
     success_url = reverse_lazy('projects:entry_list')
+
 
 class EntryUpdateView(UpdateView):
     model = Entry
